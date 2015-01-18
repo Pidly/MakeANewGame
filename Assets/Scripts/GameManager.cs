4e258969a691d88351feba5fafe70b180c98eccb
Pidly <ji773rs@gmail.com>
2015-01-17 23:52:48 -0800

    Added score and high score

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+	public static GameManager Instance;
+
+	public float pointsPerUnitTravelled = 1.0f;
+
+	public float gameSpeed = 10.0f;
+
+	private float score = 0.0f;
+	private static float highScore = 0.0f;
+
+	private bool gameOver = false;
+
+	// Use this for initialization
+	void Start () {
+		Instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (GameObject.FindGameObjectWithTag ("Player") == null) {
+			gameOver = true;
+		}
+
+		if (gameOver) {
+			if(Input.anyKey){
+				Application.LoadLevel(Application.loadedLevel);
+			}
+		}
+		if (!gameOver) {
+			score += pointsPerUnitTravelled * gameSpeed * Time.deltaTime;
+			if(score > highScore) {
+				highScore = score;			
+			}
+		}
+	}
+
+	void OnGUI(){
+		int currentHighScore = (int)highScore;
+
+		GUILayout.Label ("Score: " + ((int)score).ToString());
+		GUILayout.Label ("Highscore: " + currentHighScore.ToString ());
+
+		if (gameOver) {
+			GUILayout.Label("Game Over! Press any key to reset!");		
+		}
+	}
+}
