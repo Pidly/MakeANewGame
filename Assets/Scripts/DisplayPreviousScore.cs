034e7fad61bd35097cffa3defe0fb913df7370fb
Pidly <ji773rs@gmail.com>
2015-01-19 16:12:04 -0800

    Added music.  Kewl

---
+++ Assets/Scripts/DisplayPreviousScore.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DisplayPreviousScore : MonoBehaviour {
+	public string label;
+
+	// Use this for initialization
+	void Start () {
+		TextMesh textMesh = GetComponent<TextMesh>();
+		label = textMesh.text;
+		textMesh.text = label + GameManager.Instance.previousScore;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,42 +2,73 @@
 using System.Collections;
 
 public class GameManager : MonoBehaviour {
-	public static GameManager Instance;
+
+	public static GameManager Instance{
+		get{
+			if(_instance == null){
+				GameObject gameManager = new GameObject("GameManager");
+				_instance = gameManager.AddComponent<GameManager>();
+			}
+			return _instance;	
+		}
+	}
+	private static GameManager _instance;
 
 	public float pointsPerUnitTravelled = 1.0f;
-
 	public float gameSpeed = 10.0f;
+	public string titleScreenName = "TitleScreen";
+
+	[HideInInspector]
+	public int previousScore = 0;
 
 	private float score = 0.0f;
 	private static float highScore = 0.0f;
-
 	private bool hasSaved = false;
 	private bool gameOver = false;
 
 	// Use this for initialization
 	void Start () {
-		Instance = this;
+		if (_instance != this) {
+			if (_instance == null) {
+				_instance = this;
+			} else {
+				Destroy (gameObject);		
+			}
+		}
 		loadHighScore();
+		DontDestroyOnLoad(gameObject);
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if (GameObject.FindGameObjectWithTag ("Player") == null) {
-			gameOver = true;
-		}
+		if(Application.loadedLevelName != titleScreenName){
+			if (GameObject.FindGameObjectWithTag ("Player") == null) {
+				gameOver = true;
+			}
 
-		if (gameOver) {
-			saveHighScore();
-			if(Input.anyKey){
-				Application.LoadLevel(Application.loadedLevel);
+			if (gameOver) {
+				saveHighScore();
+				previousScore = (int)score;
+				if(Input.anyKey){
+					Application.LoadLevel(titleScreenName);
+				}
+			}
+			if (!gameOver) {
+				score += pointsPerUnitTravelled * gameSpeed * Time.deltaTime;
+				if(score > highScore) {
+					highScore = score;			
+				}
 			}
 		}
-		if (!gameOver) {
-			score += pointsPerUnitTravelled * gameSpeed * Time.deltaTime;
-			if(score > highScore) {
-				highScore = score;			
-			}
+		else{
+			resetGame();
+			//reset shit for the next game.
 		}
+	}
+
+	void resetGame(){
+		score = 0.0f;
+		gameOver = false;
 	}
 
 	void saveHighScore(){
@@ -50,13 +81,15 @@ public class GameManager : MonoBehaviour {
 	}
 
 	void OnGUI(){
-		int currentHighScore = (int)highScore;
+		if(Application.loadedLevelName != titleScreenName){
+			int currentHighScore = (int)highScore;
 
-		GUILayout.Label ("Score: " + ((int)score).ToString());
-		GUILayout.Label ("Highscore: " + currentHighScore.ToString ());
+			GUILayout.Label ("Score: " + ((int)score).ToString());
+			GUILayout.Label ("Highscore: " + currentHighScore.ToString ());
 
-		if (gameOver) {
-			GUILayout.Label("Game Over! Press any key to reset!");		
+			if (gameOver) {
+				GUILayout.Label("Game Over! Press any key to quit!");		
+			}
 		}
 	}
 }
